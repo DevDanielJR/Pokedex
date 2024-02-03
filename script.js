23a592d5d099c5747fa35db9a4ee42a20f522b88
Daniel Ribeiro <109773063+DevDanielJR@users.noreply.github.com>
2024-02-03 18:20:03 -0300

    Manipulando mais requisições
    agora solicitamos mais informações da API como o "order" e a informação das imagens.

--- script.js
+++ script.js
@@ -1,27 +1,25 @@
 
+function convertPokemonTypesToLi(pokemonTypes) {
+    return pokemonTypes.map((typeSlot) => `<li class="type">${typeSlot.type.name}</li>`)
+}
 
 function convertPokemonToLi(pokemon) {
-
     return `
-
         <!--${pokemon.name}-->
         <li class="pokemon">
-            <span class="number">#003</span>
+            <span class="number">#00${pokemon.order}</span>
             <span class="name">${pokemon.name}</span>
 
             <div class="detail">
                 <ol class="types">
-                    <li class="type">Grass</li>
-                    <li class="type">Poison</li>
+                    ${convertPokemonTypesToLi(pokemon.types).join('')}
                 </ol>
 
-                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"
+                <img src="${pokemon.sprites.other.dream_world.front_default}"
                     alt="${pokemon.name}">
-
             </div>
         </li>
     `
-
 }
 
 const pokemonList = document.getElementById('pokemonList')
